d3a6de7abc970d98368239ebc49f66009b18b777
lgsanjos <lgsanjos@gmail.com>
2014-05-19 21:47:50 -0300

    Added more tests for CategoryService

--- server/test/CategoryServiceTest.js
+++ server/test/CategoryServiceTest.js
@@ -6,10 +6,24 @@ describe('CategoryService Integration Test', function() {
 	
 	describe('getCategories', function() {
 
-		it('getAllCategories should return three categories', function(done) {
+		it('getAllCategories should return three categories', function() {
 			var categories = new CategoryService().getAllCategories();
 			categories.should.have.lengthOf(3);
-			done();
+		});
+
+		it('getSubCategoriesFromId should return one subcategory', function() {
+			var subcategories = new CategoryService().getSubCategoriesFromId(5);
+			subcategories.should.have.lengthOf(1);
+			subcategory = subcategories[0];
+
+			subcategory.should.have.property('id', '1');
+			subcategory.should.have.property('category_id', '5');
+			subcategory.should.have.property('description', 'subcategory 1 from cat 5');
+		});
+
+		it('getSubcategoriesFromId should return five subcategories', function() {
+			var categories = new CategoryService().getSubCategories();
+			categories.should.have.lengthOf(5);
 		});
 
 	});

89f5bfc49b0b7b67192865e79e4fc7c2847971ab
lgsanjos <lgsanjos@gmail.com>
2014-05-20 23:40:26 -0300

    Eliminating duplicated code on subcategory repository.

--- app/Your Market/src/br/com/sasolucoes/yourmarket/business/category/SubcategoryRepository.java
+++ app/Your Market/src/br/com/sasolucoes/yourmarket/business/category/SubcategoryRepository.java
@@ -3,15 +3,12 @@ package br.com.sasolucoes.yourmarket.business.category;
 import static br.com.sasolucoes.yourmarket.network.ServerUtils.createUrl;
 import static br.com.sasolucoes.yourmarket.network.ServerUtils.getServerUrl;
 
-import java.lang.reflect.Type;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.sasolucoes.yourmarket.network.JsonUtils;
 import br.com.sasolucoes.yourmarket.repository.Repository;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class SubcategoryRepository extends Repository {
 
@@ -22,7 +19,7 @@ public class SubcategoryRepository extends Repository {
 	public void update() {
 		URL url = getURL();
 		String result = executeService(url);
-		List<Subcategory> newSubcategories = convertFromJson(result);
+		List<Subcategory> newSubcategories = JsonUtils.convertSubcategory(result);
 		setSubcategories(newSubcategories);
 	}
 	
@@ -47,15 +44,6 @@ public class SubcategoryRepository extends Repository {
 			return;
 		
 		this.subcategories = value;
-	}
-
-	private List<Subcategory> convertFromJson(String result) {
-		Type collectionType = new TypeToken<ArrayList<Subcategory>>(){}.getType();
-		
-		Gson gson = new Gson();
-		List<Subcategory> fromJson = gson.fromJson(result, collectionType);
-		
-		return fromJson;
 	}
 
 	private URL getURL() {
--- app/Your Market/src/br/com/sasolucoes/yourmarket/network/JsonUtils.java
+++ app/Your Market/src/br/com/sasolucoes/yourmarket/network/JsonUtils.java
@@ -5,16 +5,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.sasolucoes.yourmarket.business.category.Category;
+import br.com.sasolucoes.yourmarket.business.category.Subcategory;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+@SuppressWarnings("unchecked")
 public class JsonUtils {
+	
+	@SuppressWarnings("rawtypes")
+	private static List convert(final String json, Type type) {
+		Gson gson = new Gson();
+		return gson.fromJson(json, type);
+	}
 	
 	public static List<Category> convertCategory(final String json) {
 		Type collectionType = new TypeToken<ArrayList<Category>>(){}.getType();
-		Gson gson = new Gson();
-		List<Category> fromJson = gson.fromJson(json, collectionType);
-		return fromJson;
+		return convert(json, collectionType);
+	}
+
+	public static List<Subcategory> convertSubcategory(final String json) {
+		Type collectionType = new TypeToken<ArrayList<Subcategory>>(){}.getType();
+		return convert(json, collectionType);
 	}
 }

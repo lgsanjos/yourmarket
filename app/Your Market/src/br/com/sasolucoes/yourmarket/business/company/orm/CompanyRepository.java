ffdc4815d7e51e05f7dc7667840026d3e824ec1f
Leonam Anjos <leonam.ricardo@gmail.com>
2014-06-02 21:23:13 -0300

    Displaying company data from database.

--- app/Your Market/src/br/com/sasolucoes/yourmarket/business/company/orm/CompanyRepository.java
+++ app/Your Market/src/br/com/sasolucoes/yourmarket/business/company/orm/CompanyRepository.java
@@ -3,11 +3,16 @@ package br.com.sasolucoes.yourmarket.business.company.orm;
 import static br.com.sasolucoes.yourmarket.business.company.orm.CompanyContract.CONTENT_URI;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
 public class CompanyRepository {
+	static final String TAG = "CompanyRepository";
 
 	public static int insert(Context context, Company company) {
+		Log.d(TAG, "insert");
+		
 		int result = 0;
 		
 		if (context == null || company == null)
@@ -32,6 +37,8 @@ public class CompanyRepository {
 	}
 	
 	public static int delete(Context context, long id) {
+		Log.d(TAG, "delete");
+		
 		if (context == null)
 			return 0;
 
@@ -40,10 +47,42 @@ public class CompanyRepository {
 	}
 	
 	public static int deleteAll(Context context) {
+		Log.d(TAG, "deleteAll");
+		
 		if (context == null)
 			return 0;
 
 		return context.getContentResolver().delete(CONTENT_URI, null, null);
+	}
+	
+	public static Company selectFirst(Context context) {
+		Log.d(TAG, "selectFirst");
+		
+		Cursor cursor = context.getContentResolver().query(CompanyContract.CONTENT_URI, null, null, null, null);
+	
+		Company company = getCompanyFromCursor(cursor);
+		
+		return company;
+	}
+
+	private static Company getCompanyFromCursor(Cursor cursor) {
+		Log.d(TAG, "getCompanyFromCursor");
+		
+		Company company = new Company();
+		
+		if (cursor == null)
+			return company;
+		
+		if (!cursor.moveToFirst())
+			return company;
+		
+		company.setId(cursor.getInt(cursor.getColumnIndex(CompanyContract.Column.ID)));
+		company.setName(cursor.getString(cursor.getColumnIndex(CompanyContract.Column.NAME)));
+		company.setPhone(cursor.getString(cursor.getColumnIndex(CompanyContract.Column.PHONE)));
+		company.setCellphone(cursor.getString(cursor.getColumnIndex(CompanyContract.Column.CELLPHONE)));
+		company.setEmail(cursor.getString(cursor.getColumnIndex(CompanyContract.Column.EMAIL)));
+		
+		return company;
 	}
 	
 }
--- app/Your Market/src/br/com/sasolucoes/yourmarket/business/company/ui/SellerCompanyFragment.java
+++ app/Your Market/src/br/com/sasolucoes/yourmarket/business/company/ui/SellerCompanyFragment.java
@@ -1,20 +1,63 @@
 package br.com.sasolucoes.yourmarket.business.company.ui;
 
-import br.com.sasolucoes.yourmarket.R;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import br.com.sasolucoes.yourmarket.R;
+import br.com.sasolucoes.yourmarket.business.company.orm.Company;
+import br.com.sasolucoes.yourmarket.business.company.orm.CompanyRepository;
 
 public class SellerCompanyFragment extends Fragment {
+	static String TAG = "SellerCompanyFragment"; 
+	
+	private TextView textName;
+	private TextView textPhone;
+	private TextView textCellphone;
+	private TextView textEmail;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		Log.d(TAG, "onCreateView");
 		
 		View view = inflater.inflate(R.layout.seller_company_fragment, container, false);
+		
+		textName = (TextView)view.findViewById(R.id.text_name);
+		textPhone = (TextView)view.findViewById(R.id.text_phone);
+		textCellphone = (TextView)view.findViewById(R.id.text_cellphone);
+		textEmail = (TextView)view.findViewById(R.id.text_email);
+		
 		return view;
 	}
+	
+	@Override
+	public void onResume() {
+		Log.d(TAG, "onResume");
+		
+		super.onResume();
+		updateView();
+	}
 
+	private void updateView() {
+		Log.d(TAG, "updateView");
+		
+		textName.setText("");
+		textPhone.setText("");
+		textCellphone.setText("");
+		textEmail.setText("");
+
+		Company company = CompanyRepository.selectFirst(this.getActivity());
+		
+		if (company == null)
+			return;
+		
+		textName.setText(company.getName());
+		textPhone.setText(company.getPhone());
+		textCellphone.setText(company.getCellphone());
+		textEmail.setText(company.getEmail());
+	}
 }
